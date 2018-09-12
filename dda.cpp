78950698fe276dda2056d88298fd67a901892445
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2018-09-12 19:36:36 +0530

    Draw white lines instead of green

--- dda.cpp
+++ dda.cpp
@@ -8,10 +8,10 @@ void drawLine(int x1, int y1, int x2, int y2)
 
 	if (abs(m) > 1)
 		for (int x = x1, y = y1; x < x2; x++, y = floor(y + m + 0.5))
-			putpixel(x, y, GREEN);
+			putpixel(x, y, WHITE);
 	else 
 		for (int x = x1, y = y1; y < y2; y++, x = floor(x + 1.0/m + 0.5))
-			putpixel(x, y, GREEN);
+			putpixel(x, y, WHITE);
 
 }
 
@@ -20,7 +20,7 @@ int main()
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm, "");
 	drawLine(100, 100, 400, 300);
-	drawLine(400, 100, 100, 450);
+	drawLine(100, 450, 400, 100);
 	getch();
 	return 0;
 }
