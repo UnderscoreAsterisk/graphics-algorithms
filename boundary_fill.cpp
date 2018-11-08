5ee38b23b8e3178d2ae47048af630b9c8459f53a
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2018-11-08 15:45:32 +0300

    Move comment to appropriate place

--- boundary_fill.cpp
+++ boundary_fill.cpp
@@ -8,7 +8,6 @@ void boundary_fill(int seed_x, int seed_y, unsigned int BOUNDARY_COLOR, unsigned
     stack<pair<int, int>> s;
     s.push(make_pair(seed_x, seed_y));
 
-    // Check 4-Neighbourhood
     while (!s.empty()) {
         pair<int, int> point = s.top();
         s.pop();
@@ -17,6 +16,7 @@ void boundary_fill(int seed_x, int seed_y, unsigned int BOUNDARY_COLOR, unsigned
         int y = point.second;
         putpixel(x, y, FILL_COLOR);
 
+        // Check 4-Neighbourhood
         if (getpixel(x - 1, y) != BOUNDARY_COLOR && getpixel(x - 1, y) != FILL_COLOR)
             s.push(make_pair(x - 1, y));
 
